e547d33828519b8f6d9ef9590c400c73fc75e7a9
vil-shavetail <demid.ivan@gmail.com>
2025-12-22 16:42:46 +0300

    Sprint 19 - Fixed the ScenarioCondition model.

--- telemetry/collector/src/main/java/ru/practicum/model/hub/ScenarioCondition.java
+++ telemetry/collector/src/main/java/ru/practicum/model/hub/ScenarioCondition.java
@@ -3,15 +3,15 @@ package ru.practicum.model.hub;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import ru.practicum.model.hub.enums.ActionType;
 import ru.practicum.model.hub.enums.ConditionOperation;
+import ru.practicum.model.hub.enums.ConditionType;
 
 @Getter
 @Setter
 @ToString
 public class ScenarioCondition {
     private String sensorId;
-    private ActionType type;
+    private ConditionType type;
     private ConditionOperation operation;
     private int value;
 }
